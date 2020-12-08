c1cff4c0a08334fac8d6bebb05d134996e2a1cca
Max Fedorenko <fedorenko.max163@gmail.com>
2020-12-08 20:07:20 +0200

    [Add] Class Student
    Properties:
    +FirstName
    +LastName
    +Age
    Methods:
    +Adult
    +CheckFirstName
    +CheckLastName

---
+++ Laboratory work/Lab 11.2/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_11._2
+{
+    public class Student
+    {
+        public delegate bool StudentPredicateDelegate(Student student);
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public static bool Adult(Student student) => student.Age >= 18;
+        public static bool CheckFirstName(Student student) => student.FirstName?[0] == 'A';
+        public static bool CheckLastName(Student student) => student.LastName?.Length > 3;
+    }
+}

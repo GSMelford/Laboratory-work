9f7279caefa3e2fed1ee538242292aaabf89fdf0
Max Fedorenko <fedorenko.max163@gmail.com>
2020-12-08 19:11:06 +0200

    Add override ToString

--- Laboratory work/Lab 11/Computer.cs
+++ Laboratory work/Lab 11/Computer.cs
@@ -14,5 +14,10 @@ namespace Lab_11
         public bool RAM { get; set; } = true;
         public bool Videocard { get; set; } = true;
         public bool Application { get; set; } = true;
+
+        public override string ToString() =>
+            $"\nПереустановка Windows: \t{Windows}\nЗамена материнской платы: \t{Motherboard}" +
+            $"\nЗамена блока питания: \t{PowerSupply}\nДобавление оперативной памяти: \t{RAM}\nЗамена видеокарты: \t{Videocard}" +
+            $"\nНастройка программ: \t{Application}\n";
     }
 }

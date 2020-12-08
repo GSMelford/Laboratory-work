7f2e82b2e8508265f621f3171271539578f4077e
Max Fedorenko <fedorenko.max163@gmail.com>
2020-12-08 18:06:43 +0200

    Add class Computer

---
+++ Laboratory work/Lab 11/Computer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_11
+{
+    class Computer
+    {
+        public bool Windows { get; set; } = true;
+        public bool Motherboard { get; set; } = true;
+        public bool PowerSupply { get; set; } = true;
+        public bool RAM { get; set; } = true;
+        public bool Videocard { get; set; } = true;
+        public bool Application { get; set; } = true;
+    }
+}

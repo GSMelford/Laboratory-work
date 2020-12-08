d417cc3a3df6300898d7d9c5d760eb6bf3c55335
Max Fedorenko <fedorenko.max163@gmail.com>
2020-12-08 17:31:39 +0200

    Лабораторная работа 10
    +Добавил методы расширения
    +Добавил все виды тестов для проверки кода

---
+++ Laboratory work/Lab 10/Extension.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_10
+{
+    public static class Extension
+    {
+        public static int IntReverse(this int value)
+        {
+            int reverse_number = 0;
+            while (value > 0)
+            {
+                int remainder = value % 10;
+                reverse_number = (reverse_number * 10) + remainder;
+                value = value / 10;
+            }
+
+            return reverse_number;
+        }
+        public static string StringReverse(this string value)
+        {
+            string reverse_string = string.Empty;
+            for (int i = value.Length - 1; i >= 0; i--)
+            {
+                reverse_string += value[i];
+            }
+            return reverse_string;
+        }
+        public static string StringDoubleReverse(this string value)
+        {
+            string reverse_string = string.Empty;
+            var string_arr = value.Split('.');
+
+            reverse_string += string_arr[0].StringReverse();
+            reverse_string += ".";
+            reverse_string += string_arr[1].StringReverse();
+
+            return reverse_string;
+        }
+        public static double DoubleReverse(this double value) => Double.Parse(value.ToString().StringDoubleReverse());
+        public static int[] ArrayReverse(this int[] array)
+        {
+            for (int i = 0; i < array.Length / 2; i++)
+            {
+                int temp = array[i];
+                array[i] = array[array.Length - i - 1];
+                array[array.Length - i - 1] = temp;
+            }
+            return array;
+        }
+        public static int[] ArraySort(this int[] array)
+        {
+            Array.Sort(array);
+            return array;
+        }
+    }
+}
--- Laboratory work/Lab 10/Program.cs
+++ Laboratory work/Lab 10/Program.cs
@@ -10,6 +10,31 @@ namespace Lab_10
     {
         static void Main(string[] args)
         {
+            int number = 1234;
+            Console.WriteLine($"Расширение для типа Int: {number} => {number.IntReverse()}");
+            string str = "jhagesfkj";
+            Console.WriteLine($"Расширение для типа String: {str} => {str.StringReverse()}");
+            string strDouble = "1sdafsed.sdfgas";
+            Console.WriteLine($"Расширение для типа Int: {strDouble} => {strDouble.StringDoubleReverse()}");
+            double number2 = 12.34;
+            Console.WriteLine($"Расширение для типа Int: {number2} => {number2.DoubleReverse()}");
+
+            int[] array = new int[] { 4,6,23,2,87,1,89,9,3};
+            int[] tempArray = array.ArrayReverse();
+            Console.Write("Расширение для типа Int[]: ");
+            foreach (var value in array)
+                Console.Write(value + " ");
+
+            Console.Write(" => ");
+
+            foreach (var value in tempArray)
+                Console.Write(value + " ");
+
+            Console.Write("\nРасширение для типа Int[] Сортировка: ");
+            tempArray = array.ArraySort();
+            foreach (var value in tempArray)
+                Console.Write(value + " ");
+            Console.ReadKey();
         }
     }
 }

1efa2e0bb3fa2ce4da07a766edf737822f8c57e2
Max Fedorenko <fedorenko.max163@gmail.com>
2020-12-08 20:09:54 +0200

    [Add] Class Extension
    Method:
    +FindStudents

---
+++ Laboratory work/Lab 11.2/Extension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_11._2
+{
+    public static class Extension
+    {
+        public static List<Student> FindStudents(this List<Student> students, Student.StudentPredicateDelegate studentPredicate)
+        {
+            List<Student> result = new List<Student>();
+
+            foreach (var student in students)
+            {
+                if (studentPredicate(student))
+                    result.Add(student);
+            }
+            return result; 
+        }
+    }
+}

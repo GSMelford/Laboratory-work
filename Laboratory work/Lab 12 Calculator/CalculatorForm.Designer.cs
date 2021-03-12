850a7f944e7dd010e531e507f0f87d349d5e1767
Max Fedorenko <fedorenko.max163@gmail.com>
2021-03-12 20:26:01 +0200

    [CREATE, ADD] CalculatorForm
    +Exit_Click();
    +UpPanel_MouseMove();
    +UpPanel_MouseDown();
    
    Создал форму будущего калькулятора и создал верхнюю панель с кнопкой "закрыть".

---
+++ Laboratory work/Lab 12 Calculator/CalculatorForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace Lab_12_Calculator
+{
+    partial class CalculatorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UpPanel = new System.Windows.Forms.Panel();
+            this.NameProgram = new System.Windows.Forms.Label();
+            this.Exit = new System.Windows.Forms.Button();
+            this.UpPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // UpPanel
+            // 
+            this.UpPanel.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.UpPanel.Controls.Add(this.NameProgram);
+            this.UpPanel.Controls.Add(this.Exit);
+            this.UpPanel.Location = new System.Drawing.Point(0, 0);
+            this.UpPanel.Name = "UpPanel";
+            this.UpPanel.Size = new System.Drawing.Size(190, 25);
+            this.UpPanel.TabIndex = 0;
+            this.UpPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.UpPanel_MouseDown);
+            this.UpPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.UpPanel_MouseMove);
+            // 
+            // NameProgram
+            // 
+            this.NameProgram.AutoSize = true;
+            this.NameProgram.ForeColor = System.Drawing.Color.White;
+            this.NameProgram.Location = new System.Drawing.Point(3, 3);
+            this.NameProgram.Name = "NameProgram";
+            this.NameProgram.Size = new System.Drawing.Size(92, 18);
+            this.NameProgram.TabIndex = 1;
+            this.NameProgram.Text = "Калькулятор";
+            // 
+            // Exit
+            // 
+            this.Exit.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.Exit.FlatAppearance.BorderSize = 0;
+            this.Exit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Exit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.Exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Exit.ForeColor = System.Drawing.Color.White;
+            this.Exit.Location = new System.Drawing.Point(165, 0);
+            this.Exit.Margin = new System.Windows.Forms.Padding(0);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(25, 25);
+            this.Exit.TabIndex = 1;
+            this.Exit.Text = "X";
+            this.Exit.UseVisualStyleBackColor = false;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // CalculatorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(190, 315);
+            this.Controls.Add(this.UpPanel);
+            this.Font = new System.Drawing.Font("Open Sans SemiBold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "CalculatorForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Калькулятор";
+            this.UpPanel.ResumeLayout(false);
+            this.UpPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel UpPanel;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Label NameProgram;
+    }
+}
+
---
+++ Laboratory work/Lab 12 Calculator/CalculatorForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_12_Calculator
+{
+    public partial class CalculatorForm : Form
+    {
+        private Point LastPoint;
+        public CalculatorForm()
+        {
+            InitializeComponent();
+            CreatCalculator();
+        }
+
+        private void CreatCalculator()
+        {
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void UpPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - LastPoint.X;
+                this.Top += e.Y - LastPoint.Y;
+            }
+        }
+
+        private void UpPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            LastPoint = new Point(e.X, e.Y);
+        }
+    }
+}

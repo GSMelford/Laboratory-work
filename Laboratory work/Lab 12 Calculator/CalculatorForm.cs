55a321b02d811dee08be5b8c8d591147aa12e69e
Max Fedorenko <fedorenko.max163@gmail.com>
2021-03-12 22:52:33 +0200

    [ADD] Метод CreatKeyboardCalc();

--- Laboratory work/Lab 12 Calculator/CalculatorForm.cs
+++ Laboratory work/Lab 12 Calculator/CalculatorForm.cs
@@ -13,22 +13,57 @@ namespace Lab_12_Calculator
     public partial class CalculatorForm : Form
     {
         private Point LastPoint;
+        private readonly int ButtonCounter = 16;
+        private string[] symbols = new string[] { "7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", ".", "=", "+" };
         public CalculatorForm()
         {
             InitializeComponent();
-            CreatCalculator();
+            CreatKeyboardCalc();
         }
 
-        private void CreatCalculator()
+        private void CreatKeyboardCalc()
         {
+            int size = 35;
+            int counter = 0;
+            for (int i = 0; i < ButtonCounter / 4; i++)
+            {
+                for (int j = 0; j < ButtonCounter / 4; j++)
+                {
+                    Button button = new Button();
+                    button.Location = new Point((size + 10 )* j, (size + 10) * i);
+                    button.Size = new Size(size,size);
+                    button.FlatStyle = FlatStyle.Flat;
+                    button.ForeColor = Color.White;
+                    button.BackColor = Color.FromArgb(64, 64, 64);
+                    button.Text = symbols[counter++];
+                    KeyboardCalc.Controls.Add(button);
+                }
+            }
         }
 
+        private void ButtonDelete_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            int tempNumber = 0;
+            if (int.TryParse(button.Text, out tempNumber))
+            {
+                CalculatorField.Text += button.Text;
+                return;
+            }
+                
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            /*var calculator_field = this.Controls["calculator_field"];
+            calculator_field.Text = ((Button)sender).Text;*/
+        }
         private void Exit_Click(object sender, EventArgs e)
         {
             this.Close();
         }
 
-        private void UpPanel_MouseMove(object sender, MouseEventArgs e)
+        private void TopPanel_MouseMove(object sender, MouseEventArgs e)
         {
             if(e.Button == MouseButtons.Left)
             {

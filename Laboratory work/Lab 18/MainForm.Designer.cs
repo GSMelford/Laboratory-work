e87925c58ea4b33917f5f73df3fe7f2fdf0a0413
Fedorenko Max <fedorenko.max163@gmail.com>
2021-04-24 02:23:50 +0300

    [Add] Lab 18. Task 1-3

---
+++ Laboratory work/Lab 18/MainForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace Lab_18
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CreateFolder = new System.Windows.Forms.Button();
+            this.CreateSubFolder = new System.Windows.Forms.Button();
+            this.CheckMaxSubFolders = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CreateFolder
+            // 
+            this.CreateFolder.Location = new System.Drawing.Point(12, 12);
+            this.CreateFolder.Name = "CreateFolder";
+            this.CreateFolder.Size = new System.Drawing.Size(205, 43);
+            this.CreateFolder.TabIndex = 0;
+            this.CreateFolder.Text = "Создать 100 папок";
+            this.CreateFolder.UseVisualStyleBackColor = true;
+            this.CreateFolder.Click += new System.EventHandler(this.CreateFolder_Click);
+            // 
+            // CreateSubFolder
+            // 
+            this.CreateSubFolder.Location = new System.Drawing.Point(12, 61);
+            this.CreateSubFolder.Name = "CreateSubFolder";
+            this.CreateSubFolder.Size = new System.Drawing.Size(205, 45);
+            this.CreateSubFolder.TabIndex = 3;
+            this.CreateSubFolder.Text = "Создать 100 вложеных папок";
+            this.CreateSubFolder.UseVisualStyleBackColor = true;
+            this.CreateSubFolder.Click += new System.EventHandler(this.CreateSubFolder_Click);
+            // 
+            // CheckMaxSubFolders
+            // 
+            this.CheckMaxSubFolders.Location = new System.Drawing.Point(12, 112);
+            this.CheckMaxSubFolders.Name = "CheckMaxSubFolders";
+            this.CheckMaxSubFolders.Size = new System.Drawing.Size(205, 45);
+            this.CheckMaxSubFolders.TabIndex = 4;
+            this.CheckMaxSubFolders.Text = "Узнать максимальное количество вложеных папок";
+            this.CheckMaxSubFolders.UseVisualStyleBackColor = true;
+            this.CheckMaxSubFolders.Click += new System.EventHandler(this.CheckMaxSubFolders_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(229, 168);
+            this.Controls.Add(this.CheckMaxSubFolders);
+            this.Controls.Add(this.CreateSubFolder);
+            this.Controls.Add(this.CreateFolder);
+            this.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CreateFolder;
+        private System.Windows.Forms.Button CreateSubFolder;
+        private System.Windows.Forms.Button CheckMaxSubFolders;
+    }
+}
+
---
+++ Laboratory work/Lab 18/MainForm.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_18
+{
+    public partial class MainForm : Form
+    {
+        private string FolderName = "Folder_";
+        private string TestDirectoryName = "Lab_18 Fedorenko Max Test";
+        private const int NumberSubFolders = 100;
+        private int SubFoldersCount = 0;
+        private DirectoryInfo dirInfo;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            dirInfo = new DirectoryInfo(
+                Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + "\\" + TestDirectoryName);
+
+            
+        }
+
+        private void DeleteFolders()
+        {
+            foreach (var folder in dirInfo.GetDirectories())
+            {
+                folder.Delete(true);
+            }
+        }
+        private void ShowMaxNumberSubFolder()
+        {
+            string path = "D:";
+            int count = 0;
+
+            try
+            {
+                while (true)
+                {
+                    path += @"\" + 'a';
+                    count++;
+                    Directory.CreateDirectory(path);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Максимальное количество папок " + count);
+            }
+
+            Directory.Delete(@"D:\a",true);
+        }
+        private void DeleteDeepFolders(DirectoryInfo dir)
+        {
+            if (dir.GetDirectories().Length > 0)
+            {
+                DeleteDeepFolders(dir.GetDirectories()[0]);
+            }
+            dir.Delete(true);
+        }
+        private void CreateSubFolders(DirectoryInfo dir)
+        {
+            try
+            {
+                if (SubFoldersCount == NumberSubFolders)
+                    return;
+
+                dir.CreateSubdirectory(FolderName + SubFoldersCount++);
+                CreateSubFolders(dir.GetDirectories()[0]);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Максимальное количество папок " + SubFoldersCount);
+            }
+        }
+
+        private void CreateFolder_Click(object sender, EventArgs e)
+        {
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                dirInfo.CreateSubdirectory(FolderName + i);
+            }
+
+            MessageBox.Show("Было создано 100 папок в папку \"Lab_18 Fedorenko Max Test\" на рабочем столе. " +
+                "После нажатия кнопки \"Ок\" эти папки удаляться");
+
+            if (dirInfo.Exists)
+            {
+                DeleteFolders();
+            }
+
+            dirInfo.Delete(true);
+        }
+        private void CreateSubFolder_Click(object sender, EventArgs e)
+        {
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+            else
+            {
+                DeleteFolders();
+            }
+
+            CreateSubFolders(dirInfo);
+        }
+        private void CheckMaxSubFolders_Click(object sender, EventArgs e)
+        {
+            ShowMaxNumberSubFolder();
+        }
+    }
+}

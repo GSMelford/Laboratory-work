a063994954f66b79b2a1a0090773346c244aa966
Maksym Fedorenko <m.fedorenko@upswot.com>
2021-06-06 08:46:44 +0300

    [Add] Lab 16 Empty

---
+++ Laboratory work/Lab 16/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab_16
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
